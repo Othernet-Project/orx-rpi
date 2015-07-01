3e0445bd43a662359148e4dde16190523fcbdf3c
Branko Vukelic <branko@brankovukelic.com>
2015-07-01 16:07:18 +0200

    Updated Librarian package to latest dev snapshot

--- rpi/package/python-librarian/python-librarian.mk
+++ rpi/package/python-librarian/python-librarian.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_VERSION = 0.2
-PYTHON_LIBRARIAN_SOURCE = librarian-$(PYTHON_LIBRARIAN_VERSION).tar.gz
-PYTHON_LIBRARIAN_SITE = https://github.com/Outernet-Project/librarian/releases/download/v$(PYTHON_LIBRARIAN_VERSION)/
+PYTHON_LIBRARIAN_VERSION = 27968dcab2d44059263c12c475703ecca46fc177
+PYTHON_LIBRARIAN_SOURCE = $(PYTHON_LIBRARIAN_VERSION).tar.gz
+PYTHON_LIBRARIAN_SITE = https://github.com/Outernet-Project/librarian/archive/
 PYTHON_LIBRARIAN_LICENSE = GPL
 PYTHON_LIBRARIAN_SETUP_TYPE = setuptools
 
