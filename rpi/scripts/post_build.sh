a4e48a27c6a66192e53470fe2e4c524f8013d513
Branko Vukelic <branko@brankovukelic.com>
2015-07-31 01:26:31 +0200

    Fix to post_build script to address build issue on Ubuntu

--- rpi/scripts/post_build.sh
+++ rpi/scripts/post_build.sh
@@ -5,7 +5,8 @@ set -e
 SCRIPTDIR=$(dirname "$0")
 
 # Copy version file
-cp "$SCRIPTDIR"/../{version,platform} "$TARGET_DIR/etc/"
+cp "$SCRIPTDIR"/../version "$TARGET_DIR/etc/"
+cp "$SCRIPTDIR"/../platform "$TARGET_DIR/etc/"
 
 # Escape backslashes in /etc/issue so we don't have to ruin the ASCII art
 sed -i 's|\\|\\\\|g' "$TARGET_DIR/etc/issue"
