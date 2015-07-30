6a19127be0555dc5af791330708c216eccbb0af2
Branko Vukelic <branko@brankovukelic.com>
2015-07-31 01:24:43 +0200

    Updated bottle-utils-form to 0.3.11

--- rpi/package/python-bottle-utils-form/python-bottle-utils-form.mk
+++ rpi/package/python-bottle-utils-form/python-bottle-utils-form.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_BOTTLE_UTILS_FORM_VERSION = 0.3.10
+PYTHON_BOTTLE_UTILS_FORM_VERSION = 0.3.11
 PYTHON_BOTTLE_UTILS_FORM_SOURCE = bottle-utils-form-$(PYTHON_BOTTLE_UTILS_FORM_VERSION).tar.gz
 PYTHON_BOTTLE_UTILS_FORM_SITE = http://pypi.python.org/packages/source/b/bottle-utils-form/
 PYTHON_BOTTLE_UTILS_FORM_LICENSE = GPL
