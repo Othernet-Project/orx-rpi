87bd38633bc602da73eed50ecf93fd87df44cbe3
Branko Vukelic <branko@outernet.is>
2015-11-17 13:35:48 +0100

    Added placholder package for ONDD
    Though ONDD is not normally compiled from source, we may sometimes need to
    build it. This package is a simple placeholder to make that process easier.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

---
+++ rpi/package/ondd/ondd.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# ondd
+#
+################################################################################
+
+ONDD_VERSION = 2.1.0
+ONDD_SOURCE = ondd-$(ONDD_VERSION).tar.gz
+ONDD_SITE = https://outernet.is/
+ONDD_DEPENDENCIES = openssl
+ONDD_LICENSE = PROPRIETARY
+
+define ONDD_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) INSTALL_PREFIX=$(TARGET_DIR) \
+		CC=$(TARGET_CC) clean debug release install
+endef
+
+$(eval $(generic-package))
