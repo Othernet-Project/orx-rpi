432c672198b80800c97de0222f761e37f547ee5d
Branko Vukelic <branko@outernet.is>
2015-11-26 15:52:38 +0100

    Updated librarian
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rpi/package/python-librarian/python-librarian.mk
+++ rpi/package/python-librarian/python-librarian.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_VERSION = a5037bb3b1e4912a6597b9b39b75ff1cd2d6fa7c
+PYTHON_LIBRARIAN_VERSION = 30a7d0458a0e4b96beab1612a73a5b0bc9528a00
 PYTHON_LIBRARIAN_SOURCE = $(PYTHON_LIBRARIAN_VERSION).tar.gz
 PYTHON_LIBRARIAN_SITE = https://github.com/Outernet-Project/librarian/archive
 PYTHON_LIBRARIAN_LICENSE = GPL
