a7c3c5afd7f59dc9dbf248d63abcfbd5e6a018c3
Branko Vukelic <branko@outernet.is>
2015-12-11 20:40:48 +0100

    Updated ondd-ipc
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rpi/package/python-ondd-ipc/python-ondd-ipc.mk
+++ rpi/package/python-ondd-ipc/python-ondd-ipc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_ONDD_IPC_VERSION = 1e3f648935b2c99267c16d83ba052bfaa5f0b8b1
+PYTHON_ONDD_IPC_VERSION = 720af7b0152272b243e94f958db781867b1ff4a5
 PYTHON_ONDD_IPC_SOURCE = $(PYTHON_ONDD_IPC_VERSION).tar.gz
 PYTHON_ONDD_IPC_SITE = https://github.com/Outernet-Project/ondd-ipc/archive
 PYTHON_ONDD_IPC_LICENSE = GPL
