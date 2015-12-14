a9539fb42cfb95cfa8431253142a436ca6e8a026
Branko Vukelic <branko@outernet.is>
2015-12-14 20:29:01 +0100

    Updated monitoring
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rpi/package/monitoring/monitoring.mk
+++ rpi/package/monitoring/monitoring.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MONITORING_VERSION = c94f86e088cad1df05ca809d07eed1004bcaa7df
+MONITORING_VERSION = 0e68f37bb0d5e2ac08165ca5cda1698a463f59b4
 MONITORING_SOURCE = $(MONITORING_VERSION).tar.gz
 MONITORING_SITE = https://github.com/Outernet-Project/monitoring/archive
 MONITORING_LICENSE = GPL
