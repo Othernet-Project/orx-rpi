256c9852dcbe3bb5e7b920922d5d23cef29c8d88
Branko Vukelic <branko@outernet.is>
2015-12-21 17:24:41 +0100

    Added ua-parser package
    Signed-off-by: Branko Vukelic <branko@outernet.is>

---
+++ rpi/package/python-ua-parser/python-ua-parser.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-ua-parser
+#
+################################################################################
+
+PYTHON_UA_PARSER_VERSION = 0.5.1
+PYTHON_UA_PARSER_SOURCE = ua-parser-$(PYTHON_UA_PARSER_VERSION).tar.gz
+PYTHON_UA_PARSER_SITE = https://pypi.python.org/packages/source/u/ua-parser/
+PYTHON_UA_PARSER_LICENSE = MIT
+PYTHON_UA_PARSER_LICENSE_FILES = LICENSE
+PYTHON_UA_PARSER_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
