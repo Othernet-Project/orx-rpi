da6b5feb3d263a7103c1df9c5c5fb3e269e4227d
Branko Vukelic <branko@outernet.is>
2016-01-06 18:27:26 +0100

    Updated hwd and librarian-diskspace
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rpi/package/python-hwd/python-hwd.mk
+++ rpi/package/python-hwd/python-hwd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_HWD_VERSION = 7554a190edef1db8bea172d7a424f0453e215f31
+PYTHON_HWD_VERSION = 6a8fd82d5c0d9eccd6412569cacbb0d94ca8c957
 PYTHON_HWD_SOURCE = $(PYTHON_HWD_VERSION).tar.gz
 PYTHON_HWD_SITE = https://github.com/Outernet-Project/hwd/archive
 PYTHON_HWD_LICENSE = GPL
--- rpi/package/python-librarian-diskspace/python-librarian-diskspace.mk
+++ rpi/package/python-librarian-diskspace/python-librarian-diskspace.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_DISKSPACE_VERSION = 4baf41481af0b5317fd856aebac0190fefd40d71
+PYTHON_LIBRARIAN_DISKSPACE_VERSION = 725053cbdfc2fa816e2639189c22680ab1c778dc
 PYTHON_LIBRARIAN_DISKSPACE_SOURCE = $(PYTHON_LIBRARIAN_DISKSPACE_VERSION).tar.gz
 PYTHON_LIBRARIAN_DISKSPACE_SITE = https://github.com/Outernet-Project/librarian-diskspace/archive
 PYTHON_LIBRARIAN_DISKSPACE_LICENSE = GPL
