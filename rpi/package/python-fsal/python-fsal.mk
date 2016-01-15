741ca6ef0a2d3a39a87901e725c6428858933620
Branko Vukelic <branko@outernet.is>
2016-01-15 20:58:52 +0100

    Updated fsal
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rpi/package/python-fsal/python-fsal.mk
+++ rpi/package/python-fsal/python-fsal.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_FSAL_VERSION = 2249b755722df033888b6a5600d10ff0c864fb7e
+PYTHON_FSAL_VERSION = 45a8bd643ba222d181f1fa8c178093d566d76215
 PYTHON_FSAL_SOURCE = $(PYTHON_FSAL_VERSION).tar.gz
 PYTHON_FSAL_SITE = https://github.com/Outernet-Project/fsal/archive
 PYTHON_FSAL_LICENSE = GPL
