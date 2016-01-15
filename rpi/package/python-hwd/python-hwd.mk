0ff0485204d3db9e59c774d675352c90de1c0a32
Branko Vukelic <branko@outernet.is>
2016-01-15 20:59:22 +0100

    Updated hwd package
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rpi/package/python-hwd/python-hwd.mk
+++ rpi/package/python-hwd/python-hwd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_HWD_VERSION = 6a8fd82d5c0d9eccd6412569cacbb0d94ca8c957
+PYTHON_HWD_VERSION = 022c680d45f8328beb5b3c4ed4dddd4e15ef73dc
 PYTHON_HWD_SOURCE = $(PYTHON_HWD_VERSION).tar.gz
 PYTHON_HWD_SITE = https://github.com/Outernet-Project/hwd/archive
 PYTHON_HWD_LICENSE = GPL
