37ce1cadd967599bbd1ee437915b371754241f69
Branko Vukelic <branko@outernet.is>
2016-01-29 15:08:56 +0100

    Updated librarian-diskspace
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rpi/package/python-librarian-diskspace/python-librarian-diskspace.mk
+++ rpi/package/python-librarian-diskspace/python-librarian-diskspace.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_DISKSPACE_VERSION = bbc2d393ebd723bb1ed226712f6fa6866e571258
+PYTHON_LIBRARIAN_DISKSPACE_VERSION = 03771c8ff749c40eb0832d724ba5cb66ad1edcb4
 PYTHON_LIBRARIAN_DISKSPACE_SOURCE = $(PYTHON_LIBRARIAN_DISKSPACE_VERSION).tar.gz
 PYTHON_LIBRARIAN_DISKSPACE_SITE = https://github.com/Outernet-Project/librarian-diskspace/archive
 PYTHON_LIBRARIAN_DISKSPACE_LICENSE = GPL
