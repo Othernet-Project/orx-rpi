601a095407b195c24ab381edafed225bdca709ee
Branko Vukelic <branko@outernet.is>
2016-02-01 19:16:52 +0100

    Updated librarian-ondd
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rpi/package/python-librarian-ondd/python-librarian-ondd.mk
+++ rpi/package/python-librarian-ondd/python-librarian-ondd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_ONDD_VERSION = 3bb5ec8cddd59fd8e41e5a73cefc8ae4a14e9bf9
+PYTHON_LIBRARIAN_ONDD_VERSION = 82676e120a7cd4447f659449ac0a5cb4e3daef8f
 PYTHON_LIBRARIAN_ONDD_SOURCE = $(PYTHON_LIBRARIAN_ONDD_VERSION).tar.gz
 PYTHON_LIBRARIAN_ONDD_SITE = https://github.com/Outernet-Project/librarian-ondd/archive
 PYTHON_LIBRARIAN_ONDD_LICENSE = GPL
