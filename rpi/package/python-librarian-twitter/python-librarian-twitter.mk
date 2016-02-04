852d5d0e6c95c89c554d46215de1a713ffd56414
Branko Vukelic <branko@outernet.is>
2016-02-04 16:05:16 +0100

    Updated librarian-twitter
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rpi/package/python-librarian-twitter/python-librarian-twitter.mk
+++ rpi/package/python-librarian-twitter/python-librarian-twitter.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_TWITTER_VERSION = 8d8652b10b1d86758a2fb9c97764abfb5fb8b744
+PYTHON_LIBRARIAN_TWITTER_VERSION = 71fd507b83a6be4ac06e88cbd6ec7d9f12db604f
 PYTHON_LIBRARIAN_TWITTER_SOURCE = $(PYTHON_LIBRARIAN_TWITTER_VERSION).tar.gz
 PYTHON_LIBRARIAN_TWITTER_SITE = https://github.com/Outernet-Project/librarian-twitter/archive
 PYTHON_LIBRARIAN_TWITTER_LICENSE = GPL
