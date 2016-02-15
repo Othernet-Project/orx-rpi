27f330e83461d66ef17b031a8c6ca62bce20abb9
Branko Vukelic <branko@outernet.is>
2016-02-15 19:05:47 +0100

    No need to create the card image file anymore
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rpi/scripts/post_image.sh
+++ rpi/scripts/post_image.sh
@@ -4,18 +4,3 @@ SOURCE_CFGFILE=$BR2_EXTERNAL/configs/config.txt
 CFGFILE=$BINARIES_DIR/rpi-firmware/config.txt
 
 cp $SOURCE_CFGFILE $CFGFILE
-
-cat <<EOF
-**********************************
-*                                *
-*   KERNEL IMAGE CREATED, RUN:   *
-*                                *
-*        sudo make image         *
-*        sudo make sdcard        *
-*                                *
-*              or                *
-*                                *
-*       sudo make gzimage        *
-*                                *
-**********************************
-EOF
