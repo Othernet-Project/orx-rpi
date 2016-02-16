9c1f756b7df256a4d06cf81a4506c29eded04bdf
Branko Vukelic <branko@outernet.is>
2016-02-16 14:03:40 +0100

    Updated librarian-analytics
    Changes formulation of help text
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rpi/package/python-librarian-analytics/python-librarian-analytics.mk
+++ rpi/package/python-librarian-analytics/python-librarian-analytics.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_ANALYTICS_VERSION = 0884c21de490bc21f32835b6c7d639ae87fcf6fd
+PYTHON_LIBRARIAN_ANALYTICS_VERSION = 288e60f362db5c5e2351b5d7d71696c877f73f4e
 PYTHON_LIBRARIAN_ANALYTICS_SOURCE = $(PYTHON_LIBRARIAN_ANALYTICS_VERSION).tar.gz
 PYTHON_LIBRARIAN_ANALYTICS_SITE = https://github.com/Outernet-Project/librarian-analytics/archive
 PYTHON_LIBRARIAN_ANALYTICS_LICENSE = GPL
