18016ecf848578209b467479d3bccd65b94706aa
Branko Vukelic <branko@outernet.is>
2016-02-16 13:58:22 +0100

    Updated librarian-analytics
    Fixes issue with Internet detection
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rpi/package/python-librarian-analytics/python-librarian-analytics.mk
+++ rpi/package/python-librarian-analytics/python-librarian-analytics.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_ANALYTICS_VERSION = b461aaa6d912543cc5cc3b004c64ae8429cb1647
+PYTHON_LIBRARIAN_ANALYTICS_VERSION = 0884c21de490bc21f32835b6c7d639ae87fcf6fd
 PYTHON_LIBRARIAN_ANALYTICS_SOURCE = $(PYTHON_LIBRARIAN_ANALYTICS_VERSION).tar.gz
 PYTHON_LIBRARIAN_ANALYTICS_SITE = https://github.com/Outernet-Project/librarian-analytics/archive
 PYTHON_LIBRARIAN_ANALYTICS_LICENSE = GPL
