9a56db946f75518d38fbb420bb22bb88f2820a1c
Branko Vukelic <branko@outernet.is>
2016-02-17 12:44:06 +0100

    Updated librarian-ondd
    Updated to match the facelift color scheme
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- sdcard/package/python-librarian-ondd/python-librarian-ondd.mk
+++ sdcard/package/python-librarian-ondd/python-librarian-ondd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_ONDD_VERSION = 24d3d1666213dd5fd7f2718b30ba8c6b7a49c218
+PYTHON_LIBRARIAN_ONDD_VERSION = 2bdf463e21230c0443fa6566b4d89545909f5e7d
 PYTHON_LIBRARIAN_ONDD_SOURCE = $(PYTHON_LIBRARIAN_ONDD_VERSION).tar.gz
 PYTHON_LIBRARIAN_ONDD_SITE = https://github.com/Outernet-Project/librarian-ondd/archive
 PYTHON_LIBRARIAN_ONDD_LICENSE = GPL
