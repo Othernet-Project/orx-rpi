a9508fe36f274a4dcfd7c43d381a56202b0edd83
Andrean Franc <andrean.franc@gmail.com>
2016-02-23 17:41:54 +0100

    Update python packages

--- sdcard/package/python-ondd-ipc/python-ondd-ipc.mk
+++ sdcard/package/python-ondd-ipc/python-ondd-ipc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_ONDD_IPC_VERSION = 7b9dc8709851961a64bf17ebc824d3ddb10ce79a
+PYTHON_ONDD_IPC_VERSION = 5d36689fab4356a36c417b279c85ec5f55290502
 PYTHON_ONDD_IPC_SOURCE = $(PYTHON_ONDD_IPC_VERSION).tar.gz
 PYTHON_ONDD_IPC_SITE = https://github.com/Outernet-Project/ondd-ipc/archive
 PYTHON_ONDD_IPC_LICENSE = GPL
