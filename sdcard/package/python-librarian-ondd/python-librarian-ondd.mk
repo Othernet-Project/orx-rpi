59721a8dd26088cf8311f1a48d05f54cf9f377e4
Andrean Franc <andrean.franc@gmail.com>
2016-02-25 14:31:42 +0100

    Update librarian-ondd

--- sdcard/package/python-librarian-ondd/python-librarian-ondd.mk
+++ sdcard/package/python-librarian-ondd/python-librarian-ondd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_ONDD_VERSION = 125908ba380a662dc30928bd6e944007f289051d
+PYTHON_LIBRARIAN_ONDD_VERSION = 70d333efe880d7f5a81b27f7b39b142599db5e9d
 PYTHON_LIBRARIAN_ONDD_SOURCE = $(PYTHON_LIBRARIAN_ONDD_VERSION).tar.gz
 PYTHON_LIBRARIAN_ONDD_SITE = https://github.com/Outernet-Project/librarian-ondd/archive
 PYTHON_LIBRARIAN_ONDD_LICENSE = GPL
