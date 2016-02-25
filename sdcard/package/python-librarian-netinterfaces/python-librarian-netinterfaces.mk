622e3e66fa5f56f0861c0a9b3c15013bb2a3c294
Branko Vukelic <branko@outernet.is>
2016-02-25 16:56:56 +0100

    Updated librarian-netinterfaces
    Adds the wireless access point configuration UI.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- sdcard/package/python-librarian-netinterfaces/python-librarian-netinterfaces.mk
+++ sdcard/package/python-librarian-netinterfaces/python-librarian-netinterfaces.mk
@@ -4,11 +4,10 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_NETINTERFACES_VERSION = 3c5d4a4aff5ad8a60a705a9ef1b2d7fb673a1ab9
+PYTHON_LIBRARIAN_NETINTERFACES_VERSION = c6a114a724b1473b3cfef43bc75e2af4aa5af121
 PYTHON_LIBRARIAN_NETINTERFACES_SOURCE = $(PYTHON_LIBRARIAN_NETINTERFACES_VERSION).tar.gz
 PYTHON_LIBRARIAN_NETINTERFACES_SITE = https://github.com/Outernet-Project/librarian-netinterfaces/archive
 PYTHON_LIBRARIAN_NETINTERFACES_LICENSE = GPL
-PYTHON_LIBRARIAN_NETINTERFACES_DEPENDENCIES = python-librarian-core python-librarian-dashboard python-hwd
 PYTHON_LIBRARIAN_NETINTERFACES_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
