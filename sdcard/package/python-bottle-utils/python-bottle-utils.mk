074b5df58e060203e3294e7ce54b99f2f2c01940
Branko Vukelic <branko@outernet.is>
2016-02-25 16:55:49 +0100

    Updated bottle-utils and librarian-ui
    Updated necessary to implement new netinterfaces UI
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- sdcard/package/python-bottle-utils/python-bottle-utils.mk
+++ sdcard/package/python-bottle-utils/python-bottle-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_BOTTLE_UTILS_VERSION = v0.9
+PYTHON_BOTTLE_UTILS_VERSION = aea085bc485f8d1c8370e96502eb598d9fb79499
 PYTHON_BOTTLE_UTILS_SOURCE = $(PYTHON_BOTTLE_UTILS_VERSION).tar.gz
 PYTHON_BOTTLE_UTILS_SITE = https://github.com/Outernet-Project/bottle-utils/archive
 PYTHON_BOTTLE_UTILS_LICENSE = GPL
--- sdcard/package/python-librarian-ui/python-librarian-ui.mk
+++ sdcard/package/python-librarian-ui/python-librarian-ui.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_UI_VERSION = df7adc881895c9d1dd7f4e70c23566b91eac500c
+PYTHON_LIBRARIAN_UI_VERSION = aab725544dd9c3393d23b85d03a329dfbf308800
 PYTHON_LIBRARIAN_UI_SOURCE = $(PYTHON_LIBRARIAN_UI_VERSION).tar.gz
 PYTHON_LIBRARIAN_UI_SITE = https://github.com/Outernet-Project/librarian-ui/archive
 PYTHON_LIBRARIAN_UI_LICENSE = GPL
