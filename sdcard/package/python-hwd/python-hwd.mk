82a73fc9b38aaf03b4053ac609871ccc287ee5b6
Branko Vukelic <branko@outernet.is>
2016-02-25 19:46:44 +0100

    Updated python-hwd
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- sdcard/package/python-hwd/python-hwd.mk
+++ sdcard/package/python-hwd/python-hwd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_HWD_VERSION = 022c680d45f8328beb5b3c4ed4dddd4e15ef73dc
+PYTHON_HWD_VERSION = 03ede13b90a62be4eea1deb7ea1b853093560c59
 PYTHON_HWD_SOURCE = $(PYTHON_HWD_VERSION).tar.gz
 PYTHON_HWD_SITE = https://github.com/Outernet-Project/hwd/archive
 PYTHON_HWD_LICENSE = GPL
