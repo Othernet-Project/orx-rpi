0ddae96f4edb80216d5fcf04aa7adbbf11f502d6
Branko Vukelic <branko@outernet.is>
2016-02-26 12:30:36 +0100

    Updated librarian-diskspace
    Change in notification priority
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- sdcard/package/python-librarian-diskspace/python-librarian-diskspace.mk
+++ sdcard/package/python-librarian-diskspace/python-librarian-diskspace.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_DISKSPACE_VERSION = e26c7c4cbfa3f93d8c3b097d135c7c560fea82b0
+PYTHON_LIBRARIAN_DISKSPACE_VERSION = a1078f1c810bba727e86770ba51003a08fb6644a
 PYTHON_LIBRARIAN_DISKSPACE_SOURCE = $(PYTHON_LIBRARIAN_DISKSPACE_VERSION).tar.gz
 PYTHON_LIBRARIAN_DISKSPACE_SITE = https://github.com/Outernet-Project/librarian-diskspace/archive
 PYTHON_LIBRARIAN_DISKSPACE_LICENSE = GPL
