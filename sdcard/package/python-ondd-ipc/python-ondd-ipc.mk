60b1493ba444f197abcaa499ef5aad88cdd83b9d
Branko Vukelic <branko@outernet.is>
2016-02-26 09:55:27 +0100

    Updated ondd-ipc
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- sdcard/package/python-ondd-ipc/python-ondd-ipc.mk
+++ sdcard/package/python-ondd-ipc/python-ondd-ipc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_ONDD_IPC_VERSION = 5d36689fab4356a36c417b279c85ec5f55290502
+PYTHON_ONDD_IPC_VERSION = 2ef7e12b83d9e79f7263e511456ef7ce9f412ce3
 PYTHON_ONDD_IPC_SOURCE = $(PYTHON_ONDD_IPC_VERSION).tar.gz
 PYTHON_ONDD_IPC_SITE = https://github.com/Outernet-Project/ondd-ipc/archive
 PYTHON_ONDD_IPC_LICENSE = GPL
