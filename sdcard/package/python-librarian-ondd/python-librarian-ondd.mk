398f6c65a646834f0dd84ab6f07b70793c6da22f
Branko Vukelic <branko@outernet.is>
2016-02-26 12:08:40 +0100

    Updated librarian-ondd
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- sdcard/package/python-librarian-ondd/python-librarian-ondd.mk
+++ sdcard/package/python-librarian-ondd/python-librarian-ondd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_ONDD_VERSION = 373b89384436512846d38491dbb98cba8a161912
+PYTHON_LIBRARIAN_ONDD_VERSION = 92d194341256c0895291483643248df30a0dc8da
 PYTHON_LIBRARIAN_ONDD_SOURCE = $(PYTHON_LIBRARIAN_ONDD_VERSION).tar.gz
 PYTHON_LIBRARIAN_ONDD_SITE = https://github.com/Outernet-Project/librarian-ondd/archive
 PYTHON_LIBRARIAN_ONDD_LICENSE = GPL
