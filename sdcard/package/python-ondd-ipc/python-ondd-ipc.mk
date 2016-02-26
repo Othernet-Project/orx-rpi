2e791e91c184904332e5da9fedadbd4455bcc091
Branko Vukelic <branko@outernet.is>
2016-02-26 14:20:15 +0100

    Updated ondd-ipc
    Adds methods for setting and getting output path
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- sdcard/package/python-ondd-ipc/python-ondd-ipc.mk
+++ sdcard/package/python-ondd-ipc/python-ondd-ipc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_ONDD_IPC_VERSION = 2ef7e12b83d9e79f7263e511456ef7ce9f412ce3
+PYTHON_ONDD_IPC_VERSION = 48b73e9e8fe82e58207b25196938c37a58049c75
 PYTHON_ONDD_IPC_SOURCE = $(PYTHON_ONDD_IPC_VERSION).tar.gz
 PYTHON_ONDD_IPC_SITE = https://github.com/Outernet-Project/ondd-ipc/archive
 PYTHON_ONDD_IPC_LICENSE = GPL
