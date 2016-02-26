3ad82867e84542762db307a893a14b3695b21e4a
Branko Vukelic <branko@outernet.is>
2016-02-26 19:17:31 +0100

    Updated librarian-ui and librarian-netinterfaces
    Disables autocomplete on password field in access point settings
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- sdcard/package/python-librarian-ui/python-librarian-ui.mk
+++ sdcard/package/python-librarian-ui/python-librarian-ui.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_UI_VERSION = aab725544dd9c3393d23b85d03a329dfbf308800
+PYTHON_LIBRARIAN_UI_VERSION = f7498575ea338e3e3d5240678751af7941f3185d
 PYTHON_LIBRARIAN_UI_SOURCE = $(PYTHON_LIBRARIAN_UI_VERSION).tar.gz
 PYTHON_LIBRARIAN_UI_SITE = https://github.com/Outernet-Project/librarian-ui/archive
 PYTHON_LIBRARIAN_UI_LICENSE = GPL
