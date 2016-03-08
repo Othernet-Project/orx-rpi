aba517875fa5844dc120d824c2bf1730113e389d
Andrean Franc <andrean.franc@gmail.com>
2016-03-08 15:02:35 +0100

    Update zippie to latest version

--- sdcard/package/python-zippie/python-zippie.mk
+++ sdcard/package/python-zippie/python-zippie.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_ZIPPIE_VERSION = 0a2770a3b1ec0cbaa927857f72228398f133c0e2
+PYTHON_ZIPPIE_VERSION = f041f8de0428e230471762f39dedaa0b199c3fcb
 PYTHON_ZIPPIE_SOURCE = $(PYTHON_ZIPPIE_VERSION).tar.gz
 PYTHON_ZIPPIE_SITE = https://github.com/integricho/zippie/archive
 PYTHON_ZIPPIE_LICENSE = PROPRIETARY
